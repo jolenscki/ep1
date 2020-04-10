7d1fcd9626d666a5a0f06bc67839f7eeb8050eb9
jolenscki <rodrigo0079@hotmail.com.br>
2020-04-09 21:17:19 -0300

    added more comments

--- ep1.py
+++ ep1.py
@@ -150,6 +150,22 @@ def plot_temperatures(T, lambda_val, N, delta_time, space_array, temperature_mat
     return ax
     
 def plot_heatmap(T, lambda_val, N, delta_time, space_array, time_array, temperature_matrix, title, path, filename):
+        '''
+    funcao que plota um grafico de temperaturas para cada 0.1 segundos (1/10 do tempo total)
+    @parameters:
+    - T: float, constante de tempo T
+    - lambda_val: float, constante do problema
+    - N: inteiro, numero de divisoes na barra
+    - delta_time: float, tempo total decorrido para a execucao do programa, em segundos
+    - space_array: array (1x(N+1)), contem todas as posicoes da barra
+    - time_array: array (1x(M+1)), contem todos os instantes de tempo
+    - temperature_matrix: array ((M+1)x(N+1)), matriz de temperaturas
+    - title: string, titulo a ser colocado no grafico
+    - path: string, caminho ate o local onde o arquivo sera salvo
+    - filename: string, nome com o qual o arquivo sera salvo
+    @output:
+    - ax: axis (objeto de eixo do mpl)
+    '''
     title_string = r'Mapa de {}'.format(title) + r' para a barra inteira em todos os $\mathit{ticks}$ de tempo'
     subtitle_string = r'$T = {},\; \lambda = {},\; N = {},\;$ Tempo de execução$:\; {}$ segundos'.format(T, lambda_val, N, delta_time)
     
@@ -173,12 +189,27 @@ def plot_heatmap(T, lambda_val, N, delta_time, space_array, time_array, temperat
     return ax
 
 def add_initial_ending_zeros(array):
+    '''
+    funcao para adicionar zeros nos extremos de uma array unidimensional, usada para computar a funcao f na eq 11
+    @parameters:
+    - array: array, uma array generica
+    @output:
+    - final_array: array, a array de entrada com 2 itens adicionados: inicial e final, ambos 0
+    '''
     final_array = numpy.zeros(1)
     final_array = numpy.concatenate((final_array, array))
     final_array = numpy.concatenate((final_array, numpy.zeros(1)))
     return final_array
 
 def get_D_matrix(N, lambda_val):
+    '''
+    funcao para obter a matriz tri-diagonal D a partir do valor de lambda
+    @parameters:
+    - N: inteiro, numero de divisoes na barra
+    - lambda_val: float, constante do problema
+    @output:
+    - D_matrix: matrix ((M+1)x(N+1)), matriz tridiagonal
+    '''
     a = numpy.diagflat(lambda_val * numpy.ones(N), 1)
     b = numpy.diagflat((1-2*lambda_val) * numpy.ones(N+1))
     c = numpy.diagflat(lambda_val * numpy.ones(N), -1)
@@ -188,17 +219,61 @@ def get_D_matrix(N, lambda_val):
     return D_matrix
 
 def get_e_matrix(time_array, space_array):
+    '''
+    funcao que gera uma matriz de temperaturas a partir da equacao exata de difusao
+    @parameters:
+    - time_array: array (1x(M+1)), contem todos os instantes de tempo
+    - space_array: array (1x(N+1)), contem todas as posicoes da barra
+    @output:
+    - e_matrix: array ((M+1)x(N+1)), contem os valores de temperatura calculados 
+                em todos os instantes de tempo para todas as posicoes da barra
+    '''
     s_1, t_1 = numpy.meshgrid(space_array, time_array)
-    return 10*t_1*(s_1**2)*(s_1 - 1)
+    e_matrix = 10*t_1*(s_1**2)*(s_1 - 1)
+    return e_matrix
 
 def f(space_array, k, T, M):
-    return 10*(space_array**2)*(space_array - 1) - 60*space_array*((k*T)/M) + 20*((k*T)/M)
+    '''
+    funcao f associada a eq 11
+    @parameters:
+    - space_array: array (1x(N+1)), contem todas as posicoes da barra
+    - k: inteiro, indice da linha (ou seja, o instante de tempo) em que estamos calculado f
+    - T: float, constante de tempo T
+    - M: inteiro, numero de divisoes no tempo
+    @output:
+    - f: array ((M+1)x(N+1)), contem os valores de f calculados 
+                para um instante especifico k para todas as posicoes da barra
+    '''
+    f = 10*(space_array**2)*(space_array - 1) - 60*space_array*((k*T)/M) + 20*((k*T)/M)
+    return f
 
 def get_f_matrix(space_array, k, T, M):
+    '''
+    funcao que aplica o resultado da funcao anterior na space_array de fato
+    @parameters:
+    - space_array: array (1x(N+1)), contem todas as posicoes da barra
+    - k: inteiro, indice da linha (ou seja, o instante de tempo) em que estamos calculado f
+    - T: float, constante de tempo T
+    - M: inteiro, numero de divisoes no tempo
+    @output:
+    - f_array: array ((M+1)x(N+1)), contem os valores de f calculados para um instante especifico k 
+                para as posicoes da barra exceto as extremas, estas sao substituidas por zeros
+    '''
     f_array = numpy.apply_along_axis(f, 0, space_array[1:-1], k, T, M)
     return numpy.matrix(add_initial_ending_zeros(f_array))
 
 def apply_equation_11(T, lambda_val, u, space_array):
+    '''
+    funcao que aplica a equacao 11 do enunciado do ep
+    @parameters:
+    - T: float, constante de tempo T
+    - lambda_val: float, constante do problema
+    - u: array ((M+1)x(N+1)), matriz de temperaturas (uma matriz quase nula com bordas ajustadas 
+         pelas condicoes de contorno)
+    - space_array: array (1x(N+1)), contem todas as posicoes da barra
+    @output:
+    - u: array ((M+1)x(N+1)), matriz de temperaturas com seus valores calculados segundo a equacao 11
+    '''
     M = u.shape[0] - 1
     N = u.shape[1] - 1
     D = get_D_matrix(N, lambda_val)
@@ -207,11 +282,33 @@ def apply_equation_11(T, lambda_val, u, space_array):
     return u
 
 def get_error(u, e):
+    '''
+    funcao que executa a subtracao das matrizes de solucao exata e aproximada, encontrando o erro agregado a aproximacao
+    @parameters:
+    - u: array ((M+1)x(N+1)), matriz de temperaturas
+    - e: array ((M+1)x(N+1)), matriz de temperaturas calculada a partir da funcao exata
+    @output:
+    - error_matrix: array ((M+1)x(N+1)), matriz de erro agregado a aproximacao
+    '''
     error_matrix = numpy.subtract(u, e)
     return error_matrix
 
 def run_test_vectorized(T, lambda_val, N):
-    
+    '''
+    funcao que define a rotina de execucao do programa para determinados valores de T, lambda_val e N
+    @parameters:
+    - T: float, constante de tempo T
+    - lambda_val: float, constante do problema
+    - N: inteiro, numero de divisoes feitas na barra
+    @output:
+    - M:
+    - delta_time:
+    - time_array: array (1x(M+1)), contem todos os instantes de tempo
+    - space_array: array (1x(N+1)), contem todas as posicoes da barra
+    - u: array ((M+1)x(N+1)), matriz de temperaturas
+    - exact_matrix: array ((M+1)x(N+1)), matriz de temperaturas calculada a partir da funcao exata
+    - error_matrix: array ((M+1)x(N+1)), matriz de erro agregado a aproximacao
+    '''
     start_time = time.time()
     M = get_M_parameter(T, lambda_val, N)
 
@@ -237,6 +334,13 @@ def run_test_vectorized(T, lambda_val, N):
     return M, delta_time, time_array, space_array, u, exact_matrix, error_matrix
 
 def main():
+    '''
+    funcao main, script para organizar a rotina de execucao de todos os valores de T, lambda_val e N que precisam ser testados
+    @parameters:
+    -
+    @output:
+    -
+    '''
     T = 1
     lambda_list = [0.25, 0.5]
     N_list = [10, 20, 40, 80, 160, 320, 640]
